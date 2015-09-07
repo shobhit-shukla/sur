8ff10f12a781611aa1cc6504c082956bf43727db
Sur <surhutm@gmail.com>
2015-09-07 11:36:39 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sur::Application.config.secret_key_base = 'a0e5b569c9be72baaa1dc9822deeaa0985563c0eaefb8126f4142d8edc4764b1899ea208f42a8950a0a30f5067e92f2c3eb75079b44e41566cfd0b061d9483f2'
