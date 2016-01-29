6ecde3ca99a6b96d7a2f93dde60c56cc9872af05
Sourabh <sourabh.surhut@gmail.com>
2016-01-29 18:19:58 +0530

    added geocoder config, and address model.

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,7 @@ class Address < ActiveRecord::Base
   has_many :users
   belongs_to :region
 
-  geocoded_by :full_street_address   # can also be an IP address
+  # geocoded_by :full_street_address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode  # auto-fetch address
